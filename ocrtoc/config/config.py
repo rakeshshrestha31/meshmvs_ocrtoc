24d66387a904a6c06f6c9a8d93025ca6cd710299
rakeshs <rakeshs@sfu.ca>
2022-02-06 16:27:12 -0800

    MVSNet parameters updated for OCRTOC dataset

--- ocrtoc/config/config.py
+++ ocrtoc/config/config.py
@@ -99,14 +99,14 @@ def get_ocrtoc_cfg():
 
     # the depth values are different than Pixel2Mesh and 3D-R2N2
     # the depths here are not scaled by the factor 0.57 here
-    cfg.MODEL.MVSNET.MIN_DEPTH = 0.175
-    cfg.MODEL.MVSNET.DEPTH_INTERVAL = 0.044
+    cfg.MODEL.MVSNET.MIN_DEPTH = 0.05
+    cfg.MODEL.MVSNET.DEPTH_INTERVAL = 0.03
     cfg.MODEL.MVSNET.DEPTH_REFINE = True
 
     cfg.MODEL.MVSNET.NUM_DEPTHS = 48
     cfg.MODEL.MVSNET.INPUT_IMAGE_SIZE = (224, 224)
-    cfg.MODEL.MVSNET.FOCAL_LENGTH = (248, 248)
-    cfg.MODEL.MVSNET.PRINCIPAL_POINT = (111.5, 111.5)
+    cfg.MODEL.MVSNET.FOCAL_LENGTH = (157.174325, 279.4210223)
+    cfg.MODEL.MVSNET.PRINCIPAL_POINT = (114.6402250, 110.548044)
     # loss weights
     cfg.MODEL.MVSNET.PRED_DEPTH_WEIGHT = 0.1
     cfg.MODEL.MVSNET.RENDERED_DEPTH_WEIGHT = 0.00
